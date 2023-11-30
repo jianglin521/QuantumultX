71329c643c6b5c699bb2b6215f933cc8ccd93a2f
zhuangzhuang <17610835880@163.com>
2023-11-30 09:05:16 +0800

    修改脚本

--- ql/config.py
+++ ql/config.py
@@ -37,8 +37,7 @@ yu_config = {
             {'name':'账号3','ck':'PHPSESSID=0h9qf176e9l00an361ak8l2ub7'},
             {'name':'账号4','ck':'PHPSESSID=nurjs9iam2nqti2trabkhk9o5d'},
         ], # ck设置，优先从环境变量中获取，[{'name':'德华','ck':'抓包的ck值'},{'name':'彦祖','ck':'抓包的ck值','uid':'UID_xxx'}]name值随意，方便自己辨认即可。uid是wxpusher一对一通知专属设置，其它情况不要填
-        'check_whitelist': ['鼓楼微文明','中国禁毒','漳州市龙文区妇幼保健院']
-
+        'check_whitelist': ['鼓楼微文明','中国禁毒','漳州市龙文区妇幼保健院','委-海盐政协']
     }
 """鱼儿设置完毕"""
 
@@ -57,12 +56,12 @@ kl_config={
     'blacklist':[], # 提现黑名单设置,黑名单中的账号不自动提现，填入ck中的name,['name1','name2']。
 
     'klck':[
-        {'name':'账号1','ck':'PHPSESSID=7nhpbphb5ssmdnhjg3h65uk2kr; udtauth3=c21aGCSRmfw9NcWbEH8meocJqswYo%2Bi0uihYphBipX%2BubOcsaG0q%2BjK8aY9n3PTuWdnbXwM0UJQrecXdOGnOLywZx0wkg7f31Qb0HFhEIX%2FYnQ88cMi3LhLdhkIQZGNUvuOnfMRrrk6Q1GyW67Y%2FsLh33vQJhszzk%2BnfW9BU5Yg'},
+        {'name':'账号1','ck':'PHPSESSID=gu4i0slgs8qbf6plm4g7n89m3m; udtauth3=051bK1J3e4qzkX%2BI75XWor5Snr2WnNh8ntNIcooEmkP%2B3JGP2F2fncZbND99YUfuiGCscWmpi5L9TOIONbhD8LlGG0gvG%2BrrvwRW08IQG8AEiteI07R3r80Y5go3oClLnQXfQfMu0Qz6gwN9e9WIlHjoaPshCQ8XHX97hK4MHMI'},
         {'name':'账号2','ck':'PHPSESSID=5s6k7ma8u73blut13s2j053ubl; udtauth3=eb9fz5HUhggK6kiOynUGn9Ty3Qv%2B1OU4xNGww63YlEEBA7p04IiZXcWmRx0PWTXg4w809cc0Ae8%2BY1TmZLaJ10tyGDyfYx4%2FlQN7643dm%2BmBUvG1wiJde6Zmc7G1974obpxufQ%2Fc2bjX7ytRkLOAo7zqzVdfzpWJN60LSol1Yww'},
         {'name':'账号3','ck':'PHPSESSID=to8epfaeg5f4ktst2hd18esaq0; udtauth3=8967cQBVPWiKl0xpUeR5%2F117FALvdSUIO3BGOdL%2FQrByTos8nRQjLFKf%2BfSjEHjYAgJAsHBBtokygU%2FQiZYn1ER7TYhdsbNStjgoB0tsq8%2BRgFySouuNPxc3ijsYYolt4MAtCl3O0tI%2FJZ4SosxqaCM3vl6aZra8wQDbAauFjQQ'},
         {'name':'账号4','ck':'PHPSESSID=kbpj7kas9r5pv2dc89rruh0pqg; udtauth3=ba70AsEbbq%2BcV34qotKHZbyDGgsWWPelOW%2FqxM90kgcwG0%2BUuycf7QqiSQ%2BDu9%2BGIdP2F0qFP20SrbYOphEsTJgKJODzJ1Kiud9chctNmHHk4LJMLYVRqyJlWm6Rrau4nICUdWHNwnU8I%2FupFiJTVGWkGCO%2BZDlEQ6RuL2P9%2Fxk'}
     ], # ck设置，优先从环境变量中获取，[{'name':'德华','ck':'抓包的ck值'},{'name':'彦祖','ck':'抓包的ck值','uid':'UID_xxx'}]name值随意，方便自己辨认即可。uid是wxpusher一对一通知专属设置，其它情况不要填
-    'check_whitelist': ['鼓楼微文明','中国禁毒','漳州市龙文区妇幼保健院']
+    'check_whitelist': ['鼓楼微文明','中国禁毒','漳州市龙文区妇幼保健院','委-海盐政协']
 }
 """可乐设置完毕"""
 
