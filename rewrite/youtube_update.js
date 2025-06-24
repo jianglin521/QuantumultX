9dd01ffb2edcf6ffebe59c4c9f846d47ebec7bf4
zhuangzhuang <17610835880@163.com>
2025-06-24 16:28:49 +0800

    修改

--- rewrite/youtube_update.js
+++ rewrite/youtube_update.js
@@ -1,6 +1,16 @@
-$done({
-  body: JSON.stringify({
-    enable_update: false,
-    min_version: "0.0.0"
-  })
-});
+let body = $response.body;
+try {
+  let json = JSON.parse(body);
+  
+  // 删除强制升级提示
+  delete json.contents?.twoColumnBrowseResultsRenderer?.tabs[0]?.tabRenderer?.content?.sectionListRenderer?.contents[0]?.itemSectionRenderer?.contents[0]?.infoPanelContainerRenderer?.infoPanelContentRenderer;
+  
+  // 删除顶部警告条
+  if (json.header?.messageRenderer) {
+    json.header.messageRenderer = null;
+  }
+  
+  $done({ body: JSON.stringify(json) });
+} catch (e) {
+  $done({ body });
+}
