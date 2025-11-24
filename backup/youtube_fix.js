bd31d57daeabf7b756ec90963861ff6d0184d6c5
zhuangzhuang <17610835880@163.com>
2025-11-24 21:33:10 +0800

    修改

--- backup/youtube_fix.js
+++
@@ -1,9 +0,0 @@
-let h = $request.headers;
-
-// 伪装成 YouTube 最新版本（随便改成超大版本）
-h["User-Agent"] = "com.google.ios.youtube/999.0.0 (iPhone; iOS 18.0)";
-
-// 同时伪装 YouTube Client Version（新版验证用）
-h["X-YouTube-Client-Version"] = "999.0.0";
-
-$done({ headers: h });
